bd6a5bff2e4725c30a327f2445896384688301ed
alex <alexs22221@gmail.com>
2022-06-09 08:31:20 +0300

    it turned out but works on crutches

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import logging
+
+import telegram
 from loguru import logger
 import requests as re
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, callbackcontext
+from moviepy.editor import ImageClip, concatenate_videoclips, VideoFileClip
+import numpy as np
+import cv2
+import io
+import os
 
 URL = 'https://api.telegram.org/bot'
-API_KEY = ''
+API_KEY =
+
+
+bot = telegram.Bot()
 
 
 def start(update: Update, context: callbackcontext) -> None:
@@ -35,26 +45,54 @@ def help_command(update: Update, context: callbackcontext) -> None:
 
 
 def echo(update: Update, context: callbackcontext) -> None:
+    db_images = []
     if update.message.text[-1] == '?':
         update.message.reply_text('Я не могу отвечать на вопросы я только повторбшка :с')
     else:
         for i in update.message.text.split():
-            get_pictures(CHAT_ID=update.effective_chat.id, word=i)
+            # get_pictures(CHAT_ID=update.effective_chat.id, word=i)
+            db_images.append(get_pictures(i))
+
+    clips = []
+    for i in db_images:
+        clips.append(ImageClip(i).set_duration(2))
+
+    final_clip = concatenate_videoclips([clips[0], clips[1]], method='compose')
+    final_clip.write_videofile('test_py.mp4', fps=30)
+
+    bot.send_animation(update.message.chat_id, open('test_py.mp4', 'rb'))
 
 
-def get_pictures(CHAT_ID, word) -> None:
+# def get_pictures(CHAT_ID, word) -> None:
+#     GOOGLE_PICTURES = f'https://www.googleapis.com/customsearch/v1?cx=448c512f7ed8a5a3d&key={API_KEY}' \
+#                  f''f'&lr=lang_ru&searchType=image&q=' + word
+#     pictures = []
+#     for img in re.get(GOOGLE_PICTURES).json().get('items'):
+#         pictures.append(img.get('link'))
+#
+#     re.get(f'{URL}5349612772:AAGYc8KZUolYZlwHsIAqDPiodR0CnfImnX4/sendPhoto?'
+#            f'chat_id={CHAT_ID}&photo={pictures[0]}')
+
+
+def get_pictures(word) -> list:
     GOOGLE_PICTURES = f'https://www.googleapis.com/customsearch/v1?cx=448c512f7ed8a5a3d&key={API_KEY}' \
                  f''f'&lr=lang_ru&searchType=image&q=' + word
     pictures = []
     for img in re.get(GOOGLE_PICTURES).json().get('items'):
         pictures.append(img.get('link'))
 
-    re.get(f'{URL}5349612772:AAGYc8KZUolYZlwHsIAqDPiodR0CnfImnX4/sendPhoto?'
-           f'chat_id={CHAT_ID}&photo={pictures[0]}')
+    Url_img = pictures[1]
+    Content_img = re.get(Url_img)
+
+    Frame = io.BytesIO(Content_img.content)
+    Np_frame = np.frombuffer(Frame.getvalue(), np.uint8)
+
+    image = cv2.imdecode(Np_frame, -1)
+    return image
 
 
 def main() -> None:
-    updater = Updater("TOKEN")
+    updater = Updater()
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
